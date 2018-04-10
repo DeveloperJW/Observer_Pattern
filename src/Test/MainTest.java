8afdb84496abf5040859f33f9546a67349d8ddc2
Zhaopeng Wang <zwang21@luc.edu>
2018-04-10 00:03:39 -0500

    added some Junit testing

--- src/Test/MainTest.java
+++ src/Test/MainTest.java
@@ -1,5 +1,8 @@
 package Test;
 
+import Facility.*;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
@@ -14,5 +17,27 @@ class MainTest {
 
     @org.junit.jupiter.api.Test
     void main() {
+        Facility facility= new Facility();
+        facility.setFacilityId("1");
+        facility.setFacilityName("Loyola Hall");
+        facility.setCapacity(100);
+        facility.addFacilityDetail("This is a resident hall for students. The name of the facility is Loyola Hall.");
+
+        //set up example customer for testing
+        Customer customer=new Customer();
+        customer.setCustomerId("Cust-1");
+        customer.setName("Justin Wang");
+        customer.setContactNumber("812-333-6666");
+        customer.setAddress("1000 Escalon Ave, Sunnyvale, CA, 94085");
+        customer.setSsn("123-45-6789");
+
+        //assign the example facility to customer 1
+        facility.setInUse(true);
+        facility.setCustomername(customer.geName());
+
+        FacilityManagement facilityManagement=new FacilityManagement();
+        //added customer and customer 2 to be subscribers
+        facilityManagement.Attach(customer);
+        assertEquals(facility.getCustomername(),customer.geName());
     }
 }
